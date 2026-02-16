b39c264e8e70fa46b2ac67fb6be6dcf25d0d6645
HassanTantawy-dev <hasantantawy@icloud.com>
2026-02-17 00:22:48 +0200

    push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Navbar() {
+    useEffect(() => {
+        // Initialize MDB components after component mounts
+        if (window.mdb) {
+            window.mdb.Collapse.getOrCreateInstance(document.querySelector('[data-mdb-collapse-init]'));
+            document.querySelectorAll('[data-mdb-dropdown-init]').forEach(dropdown => {
+                window.mdb.Dropdown.getOrCreateInstance(dropdown);
+            });
+        }
+    }, []);
+
     return (
         <div>
+            <>
                 {/* Navbar */}
                 <nav className="navbar navbar-expand-lg bg-body-tertiary">
                     {/* Container wrapper */}
@@ -34,17 +45,7 @@ function Navbar() {
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
                                     <a className="nav-link" href="#">
-                                        Dashboard
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">
-                                        Team
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">
-                                        Projects
+                                        Categories
                                     </a>
                                 </li>
                             </ul>
@@ -57,87 +58,13 @@ function Navbar() {
                             <a className="text-reset me-3" href="#">
                                 <i className="fas fa-shopping-cart" />
                             </a>
-                            {/* Notifications */}
-                            <div className="dropdown">
-                                <a
-                                    data-mdb-dropdown-init=""
-                                    className="text-reset me-3 dropdown-toggle hidden-arrow"
-                                    href="#"
-                                    id="navbarDropdownMenuLink"
-                                    role="button"
-                                    aria-expanded="false"
-                                >
-                                    <i className="fas fa-bell" />
-                                    <span className="badge rounded-pill badge-notification bg-danger">
-                                        1
-                                    </span>
-                                </a>
-                                <ul
-                                    className="dropdown-menu dropdown-menu-end"
-                                    aria-labelledby="navbarDropdownMenuLink"
-                                >
-                                    <li>
-                                        <a className="dropdown-item" href="#">
-                                            Some news
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a className="dropdown-item" href="#">
-                                            Another news
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a className="dropdown-item" href="#">
-                                            Something else here
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                            {/* Avatar */}
-                            <div className="dropdown">
-                                <a
-                                    data-mdb-dropdown-init=""
-                                    className="dropdown-toggle d-flex align-items-center hidden-arrow"
-                                    href="#"
-                                    id="navbarDropdownMenuAvatar"
-                                    role="button"
-                                    aria-expanded="false"
-                                >
-                                    <img
-                                        src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-                                        className="rounded-circle"
-                                        height={25}
-                                        alt="Black and White Portrait of a Man"
-                                        loading="lazy"
-                                    />
-                                </a>
-                                <ul
-                                    className="dropdown-menu dropdown-menu-end"
-                                    aria-labelledby="navbarDropdownMenuAvatar"
-                                >
-                                    <li>
-                                        <a className="dropdown-item" href="#">
-                                            My profile
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a className="dropdown-item" href="#">
-                                            Settings
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a className="dropdown-item" href="#">
-                                            Logout
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
                         </div>
                         {/* Right elements */}
                     </div>
                     {/* Container wrapper */}
                 </nav>
                 {/* Navbar */}
+            </>
 
         </div>
     )
